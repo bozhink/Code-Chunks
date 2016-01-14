f35d4b850a7695093999b0c53ae6c18f0cf0b5ee
Bozhin Karaivanov <bozhink@gmail.com>
2016-01-15 00:06:19 +0200

    F# samples added

---
+++ F#/FSharpSamples/FSharpSampleConsoleApp/Program.fs
@@ -0,0 +1,27 @@
+﻿// simple types in one line
+type Person = {FirstName:string; LastName:string}
+
+// complex types in a few lines
+type Employee = 
+  | Worker of Person
+  | Manager of Employee list
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    // one-liners
+    [1..100] |> List.sum |> printfn "sum=%d"
+
+    // no curly braces, semicolons or parentheses
+    let square x = x * x
+    let z = 42
+    let sq = square z
+    printfn "%d %d" z sq
+
+    // type inference
+    let jdoe = {FirstName="John";LastName="Doe"}
+    let worker = Worker jdoe
+
+    0 // return an integer exit code
+
---
+++ F#/FSharpSamples/FSharpSamples/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FSharpSamples
+
+type Class1() = 
+    member this.X = "F#"
