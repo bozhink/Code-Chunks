a1548f411c87b230a25559cec6e61d6535ce63c6
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-11 14:41:02 +0200

    Removed WebServiceTest.

--- C#/WebServiceTest/MyWebService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MyWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- C#/WebServiceTest/MyWebService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MyWebService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
