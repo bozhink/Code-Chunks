cef29b871c2d5facb0c51cba3e536cf65363f616
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-08 16:04:16 +0300

    Bulk insert added.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ client.index({
         body: 'Hey guys, heard from Jane lately!?',
         sent: '2015-06-29T08:30:23-0700'
     }
-}, function (error, response) {
+}, function(error, response) {
     console.log(response);
 });
 
@@ -26,6 +26,58 @@ client.get({
     index: 'emails',
     type: 'email',
     id: 1
-}, function (error, response) {
+}, function(error, response) {
     console.log(response);
 });
+
+// Index multiple emails
+client.bulk({
+    index: 'emails',
+    type: 'email',
+    body: [{
+        index: {
+            _id: 2
+        }
+    }, {
+        from: 'tony@stark.com',
+        to: ['thor@avengers.org', 'hulk@avengers.org'],
+        subject: 'Re: Greetings, Earthlings!'
+    }, {
+        index: {
+            _id: 3
+        }
+    }, {
+        from: 'thor@avengers.org',
+        to: ['tony@stark.com', 'hulk@avengers.org'],
+        subject: 'Re: Greetings, Earthlings!'
+    }, {
+        index: {
+            _id: 4
+        }
+    }, {
+        from: 'hulk@avengers.org',
+        to: ['tony@stark.com', 'thor@avengers.org'],
+        subject: 'Re: Greetings, Earthlings!'
+    }, {
+        index: {
+            _id: 5
+        }
+    }, {
+        from: 'tony@stark.com',
+        to: 'natasha@avengers.org',
+        subject: 'Fwd: Greetings, Earthlings!',
+        body: 'Banner is freaking out'
+    }, {
+        index: {
+            _id: 6
+        }
+    }, {
+        from: 'natasha@avengers.org',
+        to: 'hulk@avengers.org',
+        subject: 'Coffee?',
+        body: 'Hey Bruce, long time no see. Free for c'
+    }, ]
+}, function (error, response) {
+    console.log(JSON.stringify(response));
+});
+
