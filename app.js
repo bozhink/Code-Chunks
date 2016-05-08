6a950b66c7a7a8e07c6e03f8070149de8fe2e34c
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-08 15:38:56 +0300

    Adding email is implemented.

---
+++ app.js
@@ -0,0 +1,22 @@
+var es = require('elasticsearch'),
+    client = new es.Client({
+        host: 'localhost:9200'
+    });
+
+client.index({
+    index: 'emails',
+    type: 'email',
+    id: 1,
+    body: {
+        from: 'thor@avengers.org',
+        to: [
+            'tony@stark.com',
+            'hulk@avengers.org'
+        ],
+        subject: 'Greetings, Earthlings!',
+        body: 'Hey guys, heard from Jane lately!?',
+        sent: '2015-06-29T08:30:23-0700'
+    }
+}, function (error, response) {
+    console.log(response);
+})
