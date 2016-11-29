4b9289d1606647172babacf0dd51550c33797272
Bozhin Karaivanov <bozhink@gmail.com>
2016-11-29 23:22:02 +0200

    C#/rpn is added

---
+++ C#/rpn/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(Calculate(input));
+        }
+
+        private static long Calculate(string input)
+        {
+            var parts = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>(parts);
+
+            long result = Rec(stack);
+            return result;
+        }
+
+        private static long Rec(Stack<string> stack)
+        {
+            if (stack.Count < 1)
+            {
+                return 0L;
+            }
+
+            string item = stack.Pop();
+
+            long number;
+            bool isNumber = long.TryParse(item, out number);
+            if (isNumber)
+            {
+                return number;
+            }
+            else
+            {
+                long left, right;
+                switch (item)
+                {
+                    case "+":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left + right;
+
+                    case "-":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left - right;
+
+                    case "*":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left * right;
+
+                    case "/":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left / right;
+
+                    case "&":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left & right;
+
+                    case "|":
+                        right = Rec(stack);
+                        left = Rec(stack);
+                        return left | right;
+
+                    case "^":
+                        left = Rec(stack);
+                        right = Rec(stack);
+                        return left ^ right;
+
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
