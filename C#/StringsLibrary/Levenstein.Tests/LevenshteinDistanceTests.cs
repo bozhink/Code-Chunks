3a35ff7006acff20c1cdbea5de699cf034fa2ca6
Bozhin Karaivanov <bozhink@gmail.com>
2016-12-03 16:27:17 +0200

    C#/StringsLibrary is added

---
+++ C#/StringsLibrary/Levenstein.Tests/LevenshteinDistanceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Levensteint;
+using NUnit.Framework;
+
+// https://www.dotnetperls.com/levenshtein
+namespace Levenstein.Tests
+{
+    [TestFixture]
+    public class LevenshteinDistanceTests
+    {
+        [TestCase("ant", "aunt", 1)]
+        [TestCase("Sam", "Samantha", 5)]
+        [TestCase("clozapine", "olanzapine", 3)]
+        [TestCase("flomax", "volmax", 3)]
+        [TestCase("toradol", "tramadol", 3)]
+        [TestCase("kitten", "sitting", 3)]
+        public void LevenshteinDistance_CalculateWithNonEmptyStrings_ShouldWork(string s, string t, int dist)
+        {
+            Assert.AreEqual(dist, LevenshteinDistance.Compute(s, t));
+        }
+    }
+}
+
+
---
+++ C#/StringsLibrary/LevensteinApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using Levensteint;
+
+namespace LevensteinApplication
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Expected: 1 5 3
+            Console.WriteLine(LevenshteinDistance.Compute("aunt", "ant"));
+            Console.WriteLine(LevenshteinDistance.Compute("Sam", "Samantha"));
+            Console.WriteLine(LevenshteinDistance.Compute("flomax", "volmax"));
+        }
+    }
+}
+
+
