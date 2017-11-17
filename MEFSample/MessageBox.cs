407d24d7b6210856978905fc7a1611bd3c7b8250
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-11-17 10:31:00 +0200

    Add MEF samples

---
+++ MEFSample/MessageBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace MEFSample
+{
+    public class MessageBox
+    {
+        [Export]
+        public string MyMessage
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyyMMddhhmmssfff");
+            }
+        }
+    }
+}
--- MEFSample/Program.cs
+++ MEFSample/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Threading;
 
 namespace MEFSample
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        [Import]
+        private string message = "1";
+
+        [ImportMany]
+        private IEnumerable<int> mylist;
+
+        public static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Run();
+        }
+
+        private void Run()
+        {
+            this.Compose();
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+            Thread.Sleep(10);
+            Console.WriteLine(this.message);
+        }
+
+        private void Compose()
+        {
+            AssemblyCatalog catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.SatisfyImportsOnce(this);
         }
     }
 }
