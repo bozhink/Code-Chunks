c6ad89718d3d49573bfc795ebff8603fece3c1e5
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-01-01 22:13:24 +0200

    Add WebFormsProjects

---
+++ VS/WebFormsProjects/DataBindingWebApplication/Forms/ArrayDataBindWebForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArrayDataBindWebForm.aspx.cs" Inherits="DataBindingWebApplication.Forms.ArrayDataBindWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form" runat="server">
+    <div>
+        <asp:Label ID="MessageLabel" runat="server" Text="" />
+        <asp:CheckBoxList ID="CheckBoxListItem" runat="server" OnSelectedIndexChanged="CheckBoxListItem_SelectedIndexChanged" AutoPostBack="true"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ VS/WebFormsProjects/DataBindingWebApplication/Forms/ArrayDataBindWebForm.aspx.cs
@@ -0,0 +1,29 @@
+﻿namespace DataBindingWebApplication.Forms
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class ArrayDataBindWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.CheckBoxListItem.DataSource = this.GetStrings();
+            this.CheckBoxListItem.DataBind();
+        }
+
+        private IEnumerable<string> GetStrings()
+        {
+            return new string[]
+            {
+                "1",
+                "2",
+                "3"
+            };
+        }
+
+        protected void CheckBoxListItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = this.CheckBoxListItem.SelectedValue;
+        }
+    }
+}
