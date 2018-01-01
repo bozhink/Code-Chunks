65964f5e0d30eb5b0877f27542e68dadceae1ec9
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2018-01-01 22:01:10 +0200

    Add EdgeJSSamples

---
+++ VS/EdgeJSSamples/EdgeJSFromNode/app.js
@@ -0,0 +1,27 @@
+﻿var edge = require('edge');
+
+var helloWorld = edge.func(function () {
+    /*
+     async (input) => {
+       return ".NET" + input.ToString();
+     }
+     */
+});
+
+var foo = function () {
+    var selection = window.getSelection().getRangeAt(0);
+    var selectedText = selection.extractContents();
+    var span = document.createElement("span");
+    span.style.backgroundColor = "yellow";
+    span.appendChild(selectedText);
+    selection.insertNode(span);
+}
+
+
+helloWorld('JavaScipt', function (error, result){
+    if (error) {
+        throw error;
+    }
+
+    console.log(result);
+})
